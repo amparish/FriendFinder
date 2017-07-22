d6ec428e27d9cad8b5fe3bd70b5709766977b099
amparish <alexmparish@gmail.com>
2017-07-21 19:21:57 -0500

    routing js written

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friends = require("../data/friends.js");
+var friends = require("../data/friends");
 var path = require("path");
 
 module.exports = function(app){
@@ -6,9 +6,32 @@ module.exports = function(app){
 		res.json(friends);
 	});
 	app.post("/api/friends", function(req, res){
-		var matchFriend = {
+		var friendMatch = {
 			name: "",
 			photo: "",
+			friendDiff: 1000
 		}
+
+		var userData = req.body;
+		var userScores = userData.scores;
+		var totalDifference = 0;
+
+		for (var i=0; i<friends.length; i++){
+			console.log(friends[i].name);
+			totalDifference = 0;
+
+			for (var s=0; s<friends[i].scores[s]; s++){
+				totalDifference += Math.abs(parseInt(userScores[s]) - parseInt(friends[i].scores[s]));
+
+				if (totalDifference <= bestMatch.friendDiff){
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+					bestMatch.friendDiff = totalDifference;
+				}
+			}
+		}
+
+		friends.push(userData);
+		res.json(bestMatch);
 	});
 };
